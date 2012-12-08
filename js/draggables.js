b8ba3ea24fb59486b2474e390484f61aaad5a95c
Adrian Loh <adrianloh@gmail.com>
2012-12-09 03:37:54 +0800

    In place edit submit with ENTER. Serious glitch if you do it many times.

--- js/draggables.js
+++ js/draggables.js
@@ -49,6 +49,14 @@
 					Spank.userIsTyping = true;
 				}).editable({
 					editBy:'click',
+					onEdit: function(o) {
+						$(this).bind("keyup", function(e) {
+							if (e.keyCode===13) {       // ENTER key -- submit
+								$(this).unbind("keyup");
+								$(this).trigger("blur");
+							}
+						});
+					},
 					onSubmit: function(o) {
 						Spank.userIsTyping = false;
 						var oldname = o.previous,
