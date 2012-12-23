12ed6e6622bf8278fa700d9451c72971bdd724c7
Adrian Loh <adrianloh@gmail.com>
2012-12-23 12:03:31 +0800

    FB Login fix

--- js/fb.js
+++ js/fb.js
@@ -39,7 +39,7 @@ window.fbAsyncInit = function checkFacebookStatus() {
 			}
 //			FBUserInfo.username = "sam_beckett_92102";
 			FBUserInfo.accessToken = response.authResponse.accessToken;
-			$("#fb-login").hide();
+			//$("#fb-login").hide();
 			var q = "SELECT name,username,uid FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1=me()) and is_app_user='1'";
 			FB.api('fql',{q:q}, function(res) {
 				FBUserInfo.friends = res.data;
