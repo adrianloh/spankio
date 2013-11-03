631ffcbc8718969623615acae4bc94d2ff0903ff
Adrian Loh <adrianloh@gmail.com>
2013-11-03 10:04:48 +0800

    Code clean up

--- soundgarden.py
+++ soundgarden.py
@@ -8,6 +8,7 @@ import tornado.gen
 import tornado.httpclient
 import tornado.curl_httpclient
 from urllib import urlencode, unquote, quote_plus
+from fuzzywuzzy import fuzz
 
 
 def extract(q, terms=(), delimiter=":"):
@@ -34,14 +35,16 @@ def extract(q, terms=(), delimiter=":"):
 			return_query = dict(zip(sorted_terms_without_delimiter, clean_matched_strings))
 	return return_query
 
+
+import itertools
+
 def mx_parse_search(lyrics, page=1):
 	kwargs = dict(page=page, page_size=100, f_has_lyrics=1)
 	p = {'q_artist': ['artist', 'singer'],
 	     'q_track': ['song', 'title'],
 	     'q_lyrics': ['lyrics']
 	}
-	import itertools
-	terms = list(itertools.chain(*p.values()))
+	terms = list(itertools.chain(*p.values()))  # Flatten the list of lists
 	params = extract(lyrics, terms)
 	if params.has_key("default"):
 		kwargs['q'] = params['default']
@@ -50,8 +53,10 @@ def mx_parse_search(lyrics, page=1):
 		[kwargs.__setitem__(kw,params[s.pop()]) for (kw,s) in [(kw,set(s).intersection(param_keys)) for (kw,s) in p.items()] if s]
 	return kwargs
 
+
 def urldecode(string):
 	return unquote(string).decode('utf8')
+
 
 async_client = tornado.curl_httpclient.CurlAsyncHTTPClient()
 
@@ -60,6 +65,13 @@ MX_AGENT = "musiXmatch/211 CFNetwork/596.2.3 Darwin/12.2.0 (x86_64) (MacPro3,1)"
 
 app = open(os.path.dirname(os.path.abspath(__file__))+"/soundgarden.html").read()
 
+# Durations we need for setting various response headers
+oneday = 60 * 60 * 24
+oneweek = 60 * 60 * 24 * 7
+oneyear = 60 * 60 * 24 * 365
+
+
+# Handler for root
 class MainHandler(tornado.web.RequestHandler):
 
 	def get(self):
@@ -71,36 +83,33 @@ class MainHandler(tornado.web.RequestHandler):
 		else:
 			self.render("noagent.html")
 
+
+# Handler for facebook's channel fille
 class FBChannelFileHandler(tornado.web.RequestHandler):
 
 	def get(self):
 		# See following for why we're setting these headers:
 		# https://developers.facebook.com/blog/post/530/
-		expire = 60*60*24*365
-		self.set_header("Cache-Control", "maxage=%i" % expire)
+		self.set_header("Cache-Control", "maxage=%i" % oneyear)
 		self.set_header("Pragma", "public")
-		self.set_header("Expires", time.asctime(time.gmtime(time.time() + expire)) + " GMT")
+		self.set_header("Expires", time.asctime(time.gmtime(time.time() + oneyear)) + " GMT")
 		self.write('<script src="//connect.facebook.net/en_US/all.js"></script>')
 
 
-class LoaderIOFileHandler(tornado.web.RequestHandler):
-
-	def get(self):
-		self.write('loaderio-4ca8566477b59389b7891dc98b3805f7')
-		self.finish()
-
-
+# Handler for crossdomain file
 class XDomainFileHandler(tornado.web.RequestHandler):
 
 	def get(self):
-		self.write("""<?xml version="1.0"?><!-- http://xerxes.local:8888/crossdomain.xml --><cross-domain-policy><allow-access-from domain="*" /></cross-domain-policy>""")
+		self.write("""<?xml version="1.0"?>\
+<!-- http://moozyx.com/crossdomain.xml -->\
+<cross-domain-policy><allow-access-from domain="*" />\
+</cross-domain-policy>""")
 
 re_feat = re.compile(r" f(ea)?t[\.u]?(ring)? .+$", re.IGNORECASE)
 re_n = re.compile(r"[-']n[-']", re.IGNORECASE)
 re_punc = re.compile(r"[\?\.,\/#%\^&\*;:{}=_`~()]")
 re_dash = re.compile(r"-")
 re_white = re.compile(r" {2,}")
-
 
 def clean(s):
 	s = s.strip()
@@ -111,9 +120,6 @@ def clean(s):
 	s = re.sub(re_white, " ", s)
 	s = s.strip().lower()
 	return s
-
-from fuzzywuzzy import fuzz
-
 
 def echoKeys():
 	echo_keys = "X2OROKI8NWEDHYDTM GK22IF7L5GLQQBLEL WVAZVRHOG59HOWTNC RADDG7GQBG1VPMGDN DW9KSGOM2CLAJHTXX LIJSXT5QXXZCA3RJ9 VCNSBSPZ1ECBZHLN1 RB3XH5KYICWMGZ4MC KCCO9G9N8YE2WM6OI IALGWME7AWGMSCPXB"
@@ -126,6 +132,7 @@ def echoKeys():
 echoKeys = echoKeys()
 
 
+# Search EchoNest for artist and title
 class EchoMatchHandler(tornado.web.RequestHandler):
 
 	@tornado.gen.engine
@@ -157,6 +164,7 @@ class EchoMatchHandler(tornado.web.RequestHandler):
 			self.finish()
 
 
+# Handler for creating/updating a user's EchoNest taste profile
 class EchoTasteProfileHandler(tornado.web.RequestHandler):
 
 	@tornado.gen.engine
@@ -201,6 +209,7 @@ class EchoTasteProfileHandler(tornado.web.RequestHandler):
 			self.finish()
 
 
+# Search MusixMatch
 class MXSearchHandler(tornado.web.RequestHandler):
 
 	@tornado.gen.engine
@@ -227,7 +236,10 @@ import deathbycaptcha
 from uuid import uuid4
 
 captcha_client = deathbycaptcha.SocketClient("cockupyourbumper", "nadine")
-class Base64ImageEncoderHandler(tornado.web.RequestHandler):
+
+
+# Perform captha resolution when VK prompts
+class VKCaptchaHandler(tornado.web.RequestHandler):
 
 	@tornado.gen.engine
 	@tornado.web.asynchronous
@@ -247,8 +259,7 @@ class Base64ImageEncoderHandler(tornado.web.RequestHandler):
 		captcha = captcha_client.decode(fn, 20)
 		return callback(captcha)
 
-oneday = 86400
-oneweek = 60 * 60 * 24 * 7
+
 CACHE = {}
 
 def getSlice(aList, page, limit):
@@ -256,6 +267,7 @@ def getSlice(aList, page, limit):
 	limit = int(limit)
 	return aList[(page - 1) * limit:page * limit]
 
+# Get and cache iTunes Music Store charts
 class iTunesHandler(tornado.web.RequestHandler):
 
 	@tornado.gen.engine
@@ -293,15 +305,16 @@ class MyStaticHandler(tornado.web.StaticFileHandler):
 	def set_extra_headers(self, path):
 		self.set_header("Access-Control-Allow-Origin", "*")
 		self.set_header("Accept-Ranges", "bytes")
-		expire = 60 * 60 * 24 * 7
-		self.set_header("Cache-Control", "max-age=%i" % expire)
+		self.set_header("Cache-Control", "max-age=%i" % oneweek)
 		self.set_header("Pragma", "public")
-		self.set_header("Expires", time.asctime(time.gmtime(time.time() + expire)) + " GMT")
+		self.set_header("Expires", time.asctime(time.gmtime(time.time() + oneweek)) + " GMT")
+
 
 class NeverCacheStaticHandler(tornado.web.StaticFileHandler):
 
 	def set_extra_headers(self, path):
 		self.set_header("Cache-Control", "max-age=0, no-cache, must-revalidate")
+
 
 site_root = os.path.dirname(os.path.abspath(__file__))
 
@@ -310,14 +323,13 @@ server_settings = {'debug': True, 'gzip': True}
 application = tornado.web.Application([
 		(r"/", MainHandler),
 		(r"/channel.html", FBChannelFileHandler),
-		(r"/loaderio-4ca8566477b59389b7891dc98b3805f7.html", LoaderIOFileHandler),
 		(r"/crossdomain.xml", XDomainFileHandler),
 		(r"/(favicon.ico)", tornado.web.StaticFileHandler, {"path": site_root + "/static"}),
 		(r"/echo/match/(\d+)", EchoMatchHandler),
 		(r"/echo/taste/update", EchoTasteProfileHandler),
 		(r"/mxsearch", MXSearchHandler),
 		(r"/itunes", iTunesHandler),
-		(r"/decode/(.*)", Base64ImageEncoderHandler),
+		(r"/decode/(.*)", VKCaptchaHandler),
 		(r"/static/(.*)", MyStaticHandler, {"path": site_root + "/static"}),
 		(r"/js/(.*)", NeverCacheStaticHandler, {"path": site_root + "/js"}),
 		(r"/css/(.*)", NeverCacheStaticHandler, {"path": site_root + "/css"}),
